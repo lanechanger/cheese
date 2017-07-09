cbf69056c47502e2f81c4856a403dbbe1e418595
lanechanger <jeremy_ng_86@live.ca>
2017-07-08 20:40:19 -0400

    Create Hello.java
    Just messing around in GitHub some more with this funky smiley face

---
+++ Hello.java
@@ -0,0 +1,14 @@
+// Creates a funky face!
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("  /////");
+			System.out.println(" | o o |");
+			System.out.println("(|  ^  |)");
+			System.out.println(" | \\_/ |");
+			System.out.println("  _____");
+	}
+
+}
